529b039d25546fa65b96ee2675281ec314805b96
Serebro <sergilyin@gmail.com>
2015-01-03 22:29:14 +0200

    status codes

--- src/Mvc/RestView.php
+++ src/Mvc/RestView.php
@@ -2,218 +2,248 @@
 
 namespace PhalconRest\Mvc;
 
-use PhalconRest\Mvc\View\Engine\Json as JsonEngine;
+use Exception;
 use Phalcon\Events\ManagerInterface;
+use Phalcon\Mvc\View;
+use PhalconRest\Mvc\View\Engine\Json as JsonEngine;
 
-class RestView extends \Phalcon\Mvc\View
+class RestView extends View
 {
 
-	protected $_data;
+    protected $_data;
 
 
-	public function partial($partialPath, array $params = null){
-		if (is_array($params)) {
-			$viewParams = $this->_viewParams;
-			if (is_array($viewParams)) {
-				$this->_viewParams = array_merge($viewParams, $params);
-			} else {
-				$this->_viewParams = $params;
-			}
-		}
+    public function partial($partialPath, array $params = null)
+    {
+        if (is_array($params)) {
+            $viewParams = $this->_viewParams;
+            if (is_array($viewParams)) {
+                $this->_viewParams = array_merge($viewParams, $params);
+            } else {
+                $this->_viewParams = $params;
+            }
+        }
 
-		$this->_engineRender($this->_loadTemplateEngines(), $this->_partialsDir . $partialPath, false, false);
+        $this->_engineRender($this->_loadTemplateEngines(), $this->_partialsDir . $partialPath, false, false);
 
-		if (is_array($params)) {
-			$this->_viewParams = $viewParams;
-		}
+        if (is_array($params)) {
+            $this->_viewParams = $viewParams;
+        }
 
-		return $this->getData();
-	}
+        return $this->getData();
+    }
 
-	/**
-	 * Executes render process from dispatching data
-	 * @param string $controllerName
-	 * @param string $actionName
-	 * @param array  $params
-	 * @return $this|bool
-	 */
-	public function render($controllerName, $actionName, $params = null)
-	{
-		$engines = $this->_loadTemplateEngines();
-		$pickView = $this->_pickView;
-		if ($pickView === null) {
-			$renderView = $controllerName . '/' . $actionName;
-		} else {
-			$renderView = $pickView[0];
-			if (isset($pickView[1])) {
-				$pickViewAction = $pickView[1];
-				$layoutName = $pickViewAction;
-			}
-		}
+    /**
+     * Executes render process from dispatching data
+     * @param string $controllerName
+     * @param string $actionName
+     * @param array  $params
+     * @return $this|bool
+     */
+    public function render($controllerName, $actionName, $params = null)
+    {
+        $engines = $this->_loadTemplateEngines();
+        $pickView = $this->_pickView;
+        if ($pickView === null) {
+            $renderView = $controllerName . '/' . $actionName;
+        } else {
+            $renderView = $pickView[0];
+            if (isset($pickView[1])) {
+                $pickViewAction = $pickView[1];
+                $layoutName = $pickViewAction;
+            }
+        }
 
-		if ($this->_cacheLevel) {
-			$cache = $this->getCache();
-		} else {
-			$cache = null;
-		}
+        if ($this->_cacheLevel) {
+            $cache = $this->getCache();
+        } else {
+            $cache = null;
+        }
 
-		/** @var ManagerInterface $eventsManager */
-		$eventsManager = $this->_eventsManager;
-		if ($eventsManager) {
-			if ($eventsManager->fire('view:beforeRender', $this) === false) {
-				return false;
-			}
-		}
+        /** @var ManagerInterface $eventsManager */
+        $eventsManager = $this->_eventsManager;
+        if ($eventsManager) {
+            if ($eventsManager->fire('view:beforeRender', $this) === false) {
+                return false;
+            }
+        }
 
-		$this->_content = ob_get_contents();
+        $this->_content = ob_get_contents();
 
-		$mustClean = true;
-		$silence = true;
+        $mustClean = true;
+        $silence = true;
 
-		$this->_engineRender($engines, $renderView, $silence, $mustClean, $cache);
+        $this->_engineRender($engines, $renderView, $silence, $mustClean, $cache);
 
-		if ($eventsManager instanceof ManagerInterface) {
-			$eventsManager->fire('view:afterRender', $this);
-		}
+        if ($eventsManager instanceof ManagerInterface) {
+            $eventsManager->fire('view:afterRender', $this);
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return array
-	 * @throws \Exception
-	 */
-	protected function _loadTemplateEngines()
-	{
-		$engines = $this->_engines;
-		if ($engines === false) {
-			/** @var \Phalcon\DiInterface $dependencyInjector */
-			$dependencyInjector = $this->_dependencyInjector;
-			$engines = [];
-			$registeredEngines = $this->_registeredEngines;
-			if (!is_array($registeredEngines)) {
-				$engines['.php'] = new JsonEngine($this, $dependencyInjector);
-			} else {
-				if (!is_object($dependencyInjector)) {
-					throw new \Exception("A dependency injector container is required to obtain the application services");
-				}
+    /**
+     * @return array
+     * @throws Exception
+     */
+    protected function _loadTemplateEngines()
+    {
+        $engines = $this->_engines;
+        if ($engines === false) {
+            /** @var \Phalcon\DiInterface $dependencyInjector */
+            $dependencyInjector = $this->_dependencyInjector;
+            $engines = [];
+            $registeredEngines = $this->_registeredEngines;
+            if (!is_array($registeredEngines)) {
+                $engines['.php'] = new JsonEngine($this, $dependencyInjector);
+            } else {
+                if (!is_object($dependencyInjector)) {
+                    throw new Exception("A dependency injector container is required to obtain the application services");
+                }
 
-				$arguments = [$this, $dependencyInjector];
-				foreach ($registeredEngines as $extension => $engineService) {
-					if (is_object($engineService)) {
-						if ($engineService instanceof \Closure) {
-							$engines[$extension] = call_user_func_array($engineService, $arguments);
-						} else {
-							$engines[$extension] = $engineService;
-						}
-					} else {
-						if (!is_string($engineService)) {
-							throw new \Exception("Invalid template engine registration for extension: " . $extension);
-						}
-						$engines[$extension] = $dependencyInjector->getShared($engineService, $arguments);
-					}
-				}
-			}
+                $arguments = [$this, $dependencyInjector];
+                foreach ($registeredEngines as $extension => $engineService) {
+                    if (is_object($engineService)) {
+                        if ($engineService instanceof \Closure) {
+                            $engines[$extension] = call_user_func_array($engineService, $arguments);
+                        } else {
+                            $engines[$extension] = $engineService;
+                        }
+                    } else {
+                        if (!is_string($engineService)) {
+                            throw new Exception("Invalid template engine registration for extension: " . $extension);
+                        }
+                        $engines[$extension] = $dependencyInjector->getShared($engineService, $arguments);
+                    }
+                }
+            }
 
-			$this->_engines = $engines;
-		}
+            $this->_engines = $engines;
+        }
 
-		return $engines;
-	}
+        return $engines;
+    }
 
-	protected function _engineRender($engines, $viewPath, $silence, $mustClean, $cache = null)
-	{
-		$notExists = true;
-		$viewsDir = $this->_viewsDir;
-		$basePath = $this->_basePath;
-		$viewsDirPath = $basePath . $viewsDir . $viewPath;
+    protected function _engineRender($engines, $viewPath, $silence, $mustClean, $cache = null)
+    {
+        $notExists = true;
+        $viewsDir = $this->_viewsDir;
+        $basePath = $this->_basePath;
+        $viewsDirPath = $basePath . $viewsDir . $viewPath;
 
-		if (is_object($cache)) {
-			if ($cache->isStarted() == false) {
-				$key = null;
-				$lifetime = null;
+        if (is_object($cache)) {
+            if ($cache->isStarted() == false) {
+                $key = null;
+                $lifetime = null;
 
-				$viewOptions = $this->_options;
+                $viewOptions = $this->_options;
 
-				if (is_array($viewOptions)) {
-					if (isset($viewOptions['cache'])) {
-						$cacheOptions = $viewOptions['cache'];
-						if (is_array($cacheOptions)) {
-							if (isset($cacheOptions['key'])) {
-								$key = $cacheOptions['key'];
-							}
-							if (isset($cacheOptions['lifetime'])) {
-								$lifetime = $cacheOptions['lifetime'];
-							}
-						}
-					}
-				}
+                if (is_array($viewOptions)) {
+                    if (isset($viewOptions['cache'])) {
+                        $cacheOptions = $viewOptions['cache'];
+                        if (is_array($cacheOptions)) {
+                            if (isset($cacheOptions['key'])) {
+                                $key = $cacheOptions['key'];
+                            }
+                            if (isset($cacheOptions['lifetime'])) {
+                                $lifetime = $cacheOptions['lifetime'];
+                            }
+                        }
+                    }
+                }
 
-				if ($key === null) {
-					$key = md5($viewPath);
-				}
+                if ($key === null) {
+                    $key = md5($viewPath);
+                }
 
-				$cachedView = $cache->start($key, $lifetime);
-				if ($cachedView !== null) {
-					$this->_data = $cachedView;
-					return null;
-				}
-			}
+                $cachedView = $cache->start($key, $lifetime);
+                if ($cachedView !== null) {
+                    $this->_data = $cachedView;
 
-			if (!$cache->isFresh()) {
-				return null;
-			}
-		}
+                    return null;
+                }
+            }
 
-		$viewParams = $this->_viewParams;
-		$eventsManager = $this->_eventsManager;
+            if (!$cache->isFresh()) {
+                return null;
+            }
+        }
 
-		foreach ($engines as $extension => $engine) {
-			$viewEnginePath = $viewsDirPath . $extension;
-			if (file_exists($viewEnginePath)) {
-				if (is_object($eventsManager)) {
-					$this->_activeRenderPath = $viewEnginePath;
-					if ($eventsManager->fire('view:beforeRenderView', $this, $viewEnginePath) === false) {
-						continue;
-					}
-				}
+        $viewParams = $this->_viewParams;
+        $eventsManager = $this->_eventsManager;
 
-				$engine->render($viewEnginePath, $viewParams, $mustClean);
+        foreach ($engines as $extension => $engine) {
+            $viewEnginePath = $viewsDirPath . $extension;
+            if (file_exists($viewEnginePath)) {
+                if (is_object($eventsManager)) {
+                    $this->_activeRenderPath = $viewEnginePath;
+                    if ($eventsManager->fire('view:beforeRenderView', $this, $viewEnginePath) === false) {
+                        continue;
+                    }
+                }
 
-				$notExists = false;
-				if (is_object($eventsManager)) {
-					$eventsManager->fire('view:afterRenderView', $this);
-				}
-				break;
-			}
-		}
+                $engine->render($viewEnginePath, $viewParams, $mustClean);
 
-		if ($notExists === true) {
-			if (is_object($eventsManager)) {
-				$this->_activeRenderPath = $viewEnginePath;
-				$eventsManager->fire('view:notFoundView', $this, $viewEnginePath);
-			}
-			if (!$silence) {
-				throw new \Exception("View '" . $viewsDirPath . "' was not found in the views directory");
-			}
-		}
-	}
+                $notExists = false;
+                if (is_object($eventsManager)) {
+                    $eventsManager->fire('view:afterRenderView', $this);
+                }
+                break;
+            }
+        }
 
-	/**
-	 * @return mixed
-	 */
-	public function getData()
-	{
-		return $this->_data;
-	}
+        if ($notExists === true) {
+            if (is_object($eventsManager)) {
+                $this->_activeRenderPath = $viewEnginePath;
+                $eventsManager->fire('view:notFoundView', $this, $viewEnginePath);
+            }
+            if (!$silence) {
+                throw new Exception("View '" . $viewsDirPath . "' was not found in the views directory");
+            }
+        }
+    }
 
-	/**
-	 * @param mixed $data
-	 * @return $this
-	 */
-	public function setData($data)
-	{
-		$this->_data = $data;
-		return $this;
-	}
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->_data;
+    }
+
+    /**
+     * @param mixed $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->_data = $data;
+
+        return $this;
+    }
+
+    public function statusCodeOK()
+    {
+        $this->response->setStatusCode(200, 'OK');
+    }
+
+    public function statusCodeCreated()
+    {
+        $this->response->setStatusCode(201, 'Created');
+    }
+
+    public function statusCodeAccepted()
+    {
+        $this->response->setStatusCode(202, 'Accepted');
+    }
+
+    public function statusCodeNoContent()
+    {
+        $this->response->setStatusCode(204, 'No Content');
+    }
+
+    public function statusCodePartialContent()
+    {
+        $this->response->setStatusCode(206, 'Partial Content');
+    }
 }
