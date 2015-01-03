450b4314f177b70801ea7388c2c3416e93874755
Serebro <sergilyin@gmail.com>
2015-01-03 05:28:13 +0200

    rest router settings

---
+++ src/Mvc/Router/Rest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace PhalconRest\Mvs\Router;
+
+use Phalcon\Mvc\Router\Group;
+use Phalcon\Mvc\Router;
+
+class Rest
+{
+
+    /** @var Group */
+    protected $apiGroup;
+
+    /** @var string */
+    protected $namespace = '\Controllers\Api';
+
+    /** @var string */
+    protected $prefix = '/api';
+
+
+    /**
+     * @param string $id_regexp
+     * @return $this
+     */
+    public function init($id_regexp = '')
+    {
+        if ($id_regexp) {
+            $id_regexp = ':' . $id_regexp;
+        }
+
+        if (empty($this->apiGroup)) {
+            $this->apiGroup = new Group();
+        }
+
+        $this->apiGroup->setPrefix($this->prefix);
+        $this->apiGroup->addOptions('/{controller}', ['action' => 'options', 'namespace' => $this->namespace]);
+        $this->apiGroup->addGet    ('/{controller}', ['action' => 'index', 'namespace' => $this->namespace]);
+        $this->apiGroup->addPost   ('/{controller}', ['action' => 'post', 'namespace' => $this->namespace]);
+        $this->apiGroup->add       ('/{controller}/{action}', ['namespace' => $this->namespace]);
+        $this->apiGroup->addOptions('/{controller}/{id' . $id_regexp . '}', ['action' => 'options', 'namespace' => $this->namespace]);
+        $this->apiGroup->addGet    ('/{controller}/{id' . $id_regexp . '}', ['action' => 'get', 'namespace' => $this->namespace]);
+        $this->apiGroup->addPut    ('/{controller}/{id' . $id_regexp . '}', ['action' => 'put', 'namespace' => $this->namespace]);
+        $this->apiGroup->addPatch  ('/{controller}/{id' . $id_regexp . '}', ['action' => 'patch', 'namespace' => $this->namespace]);
+        $this->apiGroup->addHead   ('/{controller}/{id' . $id_regexp . '}', ['action' => 'head', 'namespace' => $this->namespace]);
+        $this->apiGroup->addDelete ('/{controller}/{id' . $id_regexp . '}', ['action' => 'delete', 'namespace' => $this->namespace]);
+        $this->apiGroup->add       ('/{controller}/{id' . $id_regexp . '}/{action}', ['namespace' => $this->namespace]);
+
+        return $this;
+    }
+
+    /**
+     * @param Router $router
+     * @return $this
+     */
+    public function mountTo(Router $router)
+    {
+        $router->mount($this->apiGroup);
+        return $this;
+    }
+
+    /**
+     * @return Group
+     */
+    public function getApiGroup()
+    {
+        return $this->apiGroup;
+    }
+
+    /**
+     * @param Group $group
+     * @return $this
+     */
+    public function setApiGroup(Group $group)
+    {
+        $this->apiGroup = $group;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNamespace()
+    {
+        return $this->namespace;
+    }
+
+    /**
+     * @param string $namespace
+     * @return $this
+     */
+    public function setNamespace($namespace)
+    {
+        $this->namespace = $namespace;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * @param string $prefix
+     * @return $this
+     */
+    public function setPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+        return $this;
+    }
+}
--- src/Mvc/View/Engine/Json.php
+++ src/Mvc/View/Engine/Json.php
@@ -2,25 +2,58 @@
 
 namespace PhalconRest\Mvc\View\Engine;
 
+use Exception;
+
 class Json extends \Phalcon\Mvc\View\Engine\Php
 {
 
+    /** @var int */
 	protected $json_encode_options;
 
+    protected $callback_param_name = 'callback';
 
+    protected $json_content_type = 'application/json';
+
+    protected $jsonp_content_type = 'application/javascript';
+
+
+    /**
+     * @param string $path
+     * @param array  $params
+     * @param bool   $mustClean
+     * @throws Exception
+     */
 	public function render($path, $params, $mustClean = null)
 	{
 		if (is_array($params)) {
 			extract($params);
 		}
 
-		$content = require $path;
-		if ($mustClean === true) {
-			$content = json_encode($content, $this->json_encode_options);
-			$this->_view->setContent($content);
-		} else {
-			$this->_view->setData($content);
-		}
+		$data = require $path;
+		if ($mustClean === false) {
+            $this->_view->setData($data);
+            return;
+        }
+
+        $content = json_encode($data, $this->json_encode_options);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception(json_last_error_msg());
+        }
+
+        /** @var \Phalcon\Http\RequestInterface $request */
+        $request = $this->getDI()->get('request');
+
+        /** @var \Phalcon\Http\ResponseInterface $response */
+        $response = $this->getDI()->get('response');
+
+        if ($request->has($this->callback_param_name)) {
+            $callback = $request->get($this->callback_param_name);
+            $content = "$callback($content);";
+            $response->setHeader('Content-Type', $this->jsonp_header);
+        } else {
+            $response->setHeader('Content-Type', $this->json_header);
+        }
+        $this->_view->setContent($content);
 	}
 
 	/**
@@ -40,4 +73,58 @@ class Json extends \Phalcon\Mvc\View\Engine\Php
 		$this->json_encode_options = $json_encode_options;
 		return $this;
 	}
+
+    /**
+     * @return string
+     */
+    public function getJsonContentType()
+    {
+        return $this->json_content_type;
+    }
+
+    /**
+     * @param string $json_content_type
+     * @return $this
+     */
+    public function setJsonContentType($json_content_type)
+    {
+        $this->json_content_type = $json_content_type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJsonpContentType()
+    {
+        return $this->jsonp_content_type;
+    }
+
+    /**
+     * @param string $jsonp_content_type
+     * @return $this
+     */
+    public function setJsonpContentType($jsonp_content_type)
+    {
+        $this->jsonp_content_type = $jsonp_content_type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCallbackParamName()
+    {
+        return $this->callback_param_name;
+    }
+
+    /**
+     * @param string $callback_param_name
+     * @return $this
+     */
+    public function setCallbackParamName($callback_param_name)
+    {
+        $this->callback_param_name = $callback_param_name;
+        return $this;
+    }
 }
